5fe566cb7e5ead7a6b3f1126f359dd894e914572
Syed Shah <syed.shah@dirico.com>
2022-09-14 19:19:59 +0200

    Tags file

---
+++ tags/tags.ps1
@@ -0,0 +1,36 @@
+﻿#az login and set Sunscription if it's not set
+
+#az account set --subscription ""
+#az account show
+
+#Set Tags Variables and Values
+$createdBy= "Manually Created (Old Resource)"
+$managedBy= "Dirico DevOps Team"
+$environment = "Prod"
+$lastUpdatedOn= (Get-Date).ToUniversalTime().ToString('dd-MM-yyyy HH:mm:ss UTC')
+$colorBand= "Green"
+
+#to get all resources Ids and store in an array
+$groups=$(az resource list --query [].id --output tsv)
+
+#Set some variables to support the execution
+$Count=0
+$tags = $null
+
+#Set an array for those resources which you don't wanna tag
+$arrayExclude = @("actiongroups", "domainNames", "extensions", "reservedIps", "networkWatchers","activityLogAlerts","restorePointCollections", "smartDetectorAlertRules", "certificates", "autoscalesettings", "metricalerts","scheduledqueryrules", "solutions")
+
+#loop through all resources and tag those resources which don't have any tags
+foreach ($resource in $groups)
+{
+   $exclude = $resource.split("/")[7]
+   $tags=az tag list --resource-id $resource --output json | ConvertFrom-Json
+   if  (!($tags.properties.tags -ne $null)-and !($arrayExclude -contains $exclude))
+   {
+   az tag create --resource-id $resource --tags CreatedBy=$createdBy ManagedBy=$managedBy Environment=$environment LastUpdatedOn=$lastUpdatedOn ColorBand=$colorBand
+   #$resource
+   $count=$count+1
+   }
+   
+}
+Write-Host "Total Resources Updated: $count"
